11136c29c5d531230ccda0a01df9f1056693ee8a
Rafael <rafael.acerqueirah@gmail.com>
2019-10-13 09:23:00 -0300

    Add avatar and improvements into user model

--- server/src/database/migrations/20191012134614-create-users.js
+++ server/src/database/migrations/20191012134614-create-users.js
@@ -1,37 +1,42 @@
 module.exports = {
-  up: (queryInterface, DataTypes) => {
-    return queryInterface.createTable('Users', {
+  up: (queryInterface, Sequelize) => {
+    return queryInterface.createTable('users', {
       id: {
         allowNull: false,
         autoIncrement: true,
         primaryKey: true,
-        type: DataTypes.INTEGER,
+        type: Sequelize.INTEGER,
       },
+
       name: {
         allowNull: false,
-        type: DataTypes.STRING,
+        type: Sequelize.STRING,
       },
       email: {
         allowNull: false,
-        type: DataTypes.STRING,
         unique: true,
+        type: Sequelize.STRING,
       },
-      password: {
+      avatar: {
         allowNull: false,
-        type: DataTypes.STRING,
+        type: Sequelize.STRING,
       },
-      createdAt: {
+      password_hash: {
         allowNull: false,
-        type: DataTypes.DATE,
+        type: Sequelize.STRING,
       },
-      updatedAt: {
+      created_at: {
         allowNull: false,
-        type: DataTypes.DATE,
+        type: Sequelize.DATE,
+      },
+      updated_at: {
+        allowNull: false,
+        type: Sequelize.DATE,
       },
     });
   },
 
-  down: queryInterface => {
-    return queryInterface.dropTable('Users');
+  down: (queryInterface, Sequelize) => {
+    return queryInterface.dropTable('users');
   },
 };
