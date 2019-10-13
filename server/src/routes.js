2f9f601d862b2ad564f026fab1c12131568d5c97
Rafael <rafael.acerqueirah@gmail.com>
2019-10-13 10:04:44 -0300

    User can make a registration

--- server/src/routes.js
+++ server/src/routes.js
@@ -2,6 +2,10 @@ import { Router } from 'express';
 
 const routes = new Router();
 
+const UserController = require('./app/controllers/UserController');
+
 routes.get('/', (req, res) => res.json({ message: 'Welcome to Omni CLI' }));
+
+routes.post('/registration', UserController.create);
 
 export default routes;
